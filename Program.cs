851d95ee970f5c6e0a74861e12add2ec750b9bdb
Ariel Lisboa Lopes <101740188+ArielLopes888@users.noreply.github.com>
2022-06-09 17:10:57 -0300

    Compra mínima e máxima Atividade 1/ Aula 7
    Usamos função e condicional

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿// Cotação do dólar no dia 3 de maio de 2022.
+double cotacaoDoDolarHoje = 4.97;
+const double VALOR_MINIMO_DE_COMPRA_EM_DOLAR = 100;
+const double VALOR_MAXIMO_DE_COMPRA_EM_DOLAR = 2500;
+
+string converterRealParaDolar(double valorDaCompraEmReais)
+{
+    double valorConvertido = valorDaCompraEmReais / cotacaoDoDolarHoje;
+    string mensagem = "";
+    if (valorConvertido < VALOR_MINIMO_DE_COMPRA_EM_DOLAR)
+        {
+        mensagem = $" A compra mínima permitida é de ${VALOR_MINIMO_DE_COMPRA_EM_DOLAR}";
+    }
+    else if (valorConvertido > VALOR_MAXIMO_DE_COMPRA_EM_DOLAR)
+        {
+        mensagem = $" A compra máxima permitida é de ${VALOR_MAXIMO_DE_COMPRA_EM_DOLAR}";
+    }
+    else
+    {
+        mensagem = $"Você pode comprar ${valorConvertido}";
+    }
+    return mensagem;
+}
+
+
+// Utilize as mensagens a seguir como base do retorno esperado:
+// A compra mínima permitida é de $ XXX,XX.
+// A compra máxima permitida é de $ XXXX,XX.
+// Você pode comprar $ XXX,XX.
+Console.WriteLine(converterRealParaDolar(10.00));
+
